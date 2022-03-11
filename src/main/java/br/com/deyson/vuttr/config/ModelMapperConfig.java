c598cd3b18114b2c8e421d7c5f91c6f0d3baa972
Deyson <deyson.trindade@smilego.com.br>
2022-03-11 18:15:59 -0300

    adding ModelMapperConfig

---
+++ src/main/java/br/com/deyson/vuttr/config/ModelMapperConfig.java
@@ -0,0 +1,19 @@
+package br.com.deyson.vuttr.config;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper mapper() {
+
+        final ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        return modelMapper;
+    }
+}
