a46db8439c479035dacf27d920698677477c1633
Deyson <deyson.trindade@smilego.com.br>
2022-03-11 18:12:53 -0300

    refactoring SwaggerConfig

--- src/main/java/br/com/deyson/vuttr/config/SwaggerConfig.java
+++ src/main/java/br/com/deyson/vuttr/config/SwaggerConfig.java
@@ -1,25 +1,25 @@
 package br.com.deyson.vuttr.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.GroupedOpenApi;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import br.com.deyson.vuttr.config.Properties;
 
 @Configuration
+@AllArgsConstructor
 public class SwaggerConfig {
 
     public Properties properties;
 
 
     @Bean
-    public GroupedOpenApi publicApi() {
-        return GroupedOpenApi.builder()
-                .addOpenApiCustomiser(openApi -> openApi.info(new Info().title(properties.getInfo().getApp().getName()).version(properties.getInfo().getApp().getVersion()).description(properties.getInfo().getApp().getDescription())))
-                .group("endpoints")
-                .pathsToMatch("/monitor/")
-                .build();
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title(properties.getInfo().getApp().getName())
+                        .version(properties.getInfo().getApp().getVersion())
+                        .description(properties.getInfo().getApp().getDescription()));
     }
 
 }
