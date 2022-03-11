293ca879d07b9d4254e65528b4d03d00477783c6
Deyson <deyson.trindade@smilego.com.br>
2022-03-11 18:14:48 -0300

    creating models

---
+++ src/main/java/br/com/deyson/vuttr/models/TagModel.java
@@ -0,0 +1,9 @@
+package br.com.deyson.vuttr.models;
+
+import java.util.UUID;
+
+public class TagModel {
+
+    private UUID toolId;
+
+}
---
+++ src/main/java/br/com/deyson/vuttr/models/ToolModel.java
@@ -0,0 +1,22 @@
+package br.com.deyson.vuttr.models;
+
+import lombok.Data;
+
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class ToolModel {
+
+    private UUID id;
+
+    private String title;
+
+    private String link;
+
+    private String description;
+
+    private List<TagModel> tags;
+
+
+}
